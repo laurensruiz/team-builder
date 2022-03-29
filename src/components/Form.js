79630a2f5ce66693cc81d1a9d549a63102ee966d
laurensruiz <laurenruiz97@yahoo.com>
2022-03-29 12:09:45 -0400

    setup form js

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,9 +2,44 @@ import React from 'react';
 
 const Form = (props) => {
 
+    const handleChange = event => {
+        const {name, value} = event.target;
+
+        props.change(name, value);
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        props.submit();
+    }
+
     return(
-        <form>
-        
+        <form onSubmit={handleSubmit}>
+            <label>Name
+            <input
+                placeholder="Enter name here:"
+                value={values.name}
+                name="name"
+                onChange={handleChange}
+            />
+            </label>
+            <label>Email
+            <input
+                placeholder="Enter email here:"
+                value={values.email}
+                name="email"
+                onChange={handleChange}
+            />
+            </label>
+            <label>Role
+            <input
+                placeholder="Enter role here:"
+                value={values.role}
+                name="role"
+                onChange={handleChange}
+            />
+            </label>
+            <input type="submit" value="Create Team" />
         </form>
 
     )
